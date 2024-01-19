b306f0801766ffb73588d97d53482feae0606a61
onqqr <dnbyaniss@gmail.com>
2024-01-19 11:08:01 +0300

    feat: solved getBalanceIndex

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -218,8 +218,25 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length - 1; i += 1) {
+    let leftNumber = 0;
+    let rightNumber = 0;
+
+    for (let j = 0; j < arr.length; j += 1) {
+      if (j < i) {
+        leftNumber += arr[j];
+      } else if (j > i) {
+        rightNumber += arr[j];
+      }
+    }
+
+    if (leftNumber === rightNumber) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
