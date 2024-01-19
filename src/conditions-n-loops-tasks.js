dc547934cde3e9d78df68ff657a033d6a800747e
onqqr <dnbyaniss@gmail.com>
2024-01-19 10:53:28 +0300

    feat: solved isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -88,8 +88,17 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) {
+    return false;
+  }
+  if (a === b || b === c || c === a) {
+    if (a > b + c || b > a + c || c > a + b) {
+      return false;
+    }
+    return true;
+  }
+  return false;
 }
 
 /**
